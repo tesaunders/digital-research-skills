9629fef3468a8c6e026a489a7dca1b653aac4a1b
Tom Saunders <tom.saunders@auckland.ac.nz>
2024-05-09 17:46:55 +1200

    Expand content

--- R/dc-follow-up.r
+++ R/dc-follow-up.r
@@ -1,6 +1,12 @@
-
+# Load tidyverse
 
 library(tidyverse)
+
+# Check which datasets are built in
+
+data()
+
+# Look up help for mpg dataset
 
 ?mpg
 
@@ -8,18 +14,33 @@ mileage <- mpg
 
 # dplyr
 
-### slice_head()
+## slice_head(), slice_tail()
+
+mileage |> 
+  slice_head()
 
 mileage |> 
   slice_head(n = 5)
+
+mileage |>
+  group_by(manufacturer) |>
+  summarise(best = max(hwy)) |> 
+  arrange(desc(best)) |> 
+  slice_head(n = 10)
+
+## case_when()
 
 top_10 <- mileage |> 
   arrange(desc(hwy)) |> 
   slice_head(n = 10)
 
-### case_when()
-
 unique(top_10$year)
+
+top_10 |> 
+  mutate(
+    decade = case_when(year < 2000 ~ "1990s",
+                       .default = "2000s")
+  )
 
 top_10 <- top_10 |> 
   mutate(
@@ -30,10 +51,20 @@ top_10 <- top_10 |>
 top_10 |> 
   filter(decade == "1990s")
 
-### left_join()
+top_10 <- top_10 |> 
+  mutate(
+    buy = case_when(str_detect(trans, "auto") & fl == "r" ~ "yes",
+                    .default = "no")
+  )
+
+## left_join()
 
 manufacturers <- data.frame(manufacturer = unique(top_10$manufacturer),
                    hq = c("Germany", "Japan", "Japan"))
 
 combined <- top_10 |> 
   left_join(manufacturers)
+
+combined <- top_10 |> 
+  left_join(manufacturers) |> 
+  relocate(hq, .before = model)
